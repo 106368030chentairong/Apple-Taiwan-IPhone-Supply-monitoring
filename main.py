1a58fbea5c47562bbc136b5114c77686ba76148f
106368030chentairong <t106368030@ntut.edu.tw>
2022-10-06 22:52:05 +0800

    update

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ def foramt_mag(response,msg):
     return msg
 
 def get_order():
-    check_list = list(os.environ.get("check_list"))
+    check_list = list(str(os.environ.get("check_list")))
     msg = ""
     for partNumber in check_list:
+        print(partNumber)
         #product_number_list = iphone_check_api().get_partNumber("iphone14")
         response = iphone_check_api().check_store_state(partNumber)
         msg += foramt_mag(response,msg)
