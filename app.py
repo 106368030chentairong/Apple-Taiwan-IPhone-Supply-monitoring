8dbac2f89aa339b07c3a66383c9f40a7ccb40549
106368030chentairong <t106368030@ntut.org.tw>
2022-10-07 11:30:22 +0800

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, abort, request
+from lib.iphone_check import *
 
 app = Flask(__name__)
 
@@ -9,3 +10,8 @@ app = Flask(__name__)
 def callback():
     if request.method == "GET":
         return "Hello Heroku"
+
+
+@app.route("/<str:id>", methods=['GET'])
+def index_id(id):
+    return iphone_check_api().get_partNumber(id)
